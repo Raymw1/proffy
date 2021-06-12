d53b14185c4884f2916aa2b57af3418829630378
Rayan Wilbert <rayan.rmw@gmail.com>
2021-06-11 22:04:05 -0300

    Message of no proffys

--- src/server.js
+++ src/server.js
@@ -7,53 +7,73 @@ nunjucks.configure('src/views',{
 })
 
 server.use(express.static("public"));
+const Database = require("./database/db");
 
-const proffys = [
-    {
-        name: "Diego Fernandes", 
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-        whatsapp: "999999999", 
-        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-        subject: "Química", 
-        cost: "20", 
-        weekday: [0], 
-        time_from: 720, 
-        time_to: 1220
-    },
-    {
-        name: "Mayk Brito", 
-        avatar: "https://avatars.githubusercontent.com/u/6643122?v=4", 
-        whatsapp: "999999999", 
-        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-        subject: "Química", 
-        cost: "20", 
-        weekday: [0], 
-        time_from: 720, 
-        time_to: 1220
-    },
-    {
-        name: "Rafaella Ballerini", 
-        avatar: "https://avatars.githubusercontent.com/u/54322854?v=4", 
-        whatsapp: "999999999", 
-        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-        subject: "Química", 
-        cost: "20", 
-        weekday: [0], 
-        time_from: 720, 
-        time_to: 1220
-    }
-];
 
-const subjects = ["Artes", "Biologia", "Ciências", "Educação Física", "Física", "Geografia", "História", "Matemática", "Português", "Química"];
-const weekdays = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+// const proffys = [
+//     {
+//         name: "Diego Fernandes", 
+//         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+//         whatsapp: "999999999", 
+//         bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+//         subject: "Química", 
+//         cost: "20", 
+//         weekday: [0], 
+//         time_from: 720, 
+//         time_to: 1220
+//     },
+//     {
+//         name: "Mayk Brito", 
+//         avatar: "https://avatars.githubusercontent.com/u/6643122?v=4", 
+//         whatsapp: "999999999", 
+//         bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+//         subject: "Química", 
+//         cost: "20", 
+//         weekday: [0], 
+//         time_from: 720, 
+//         time_to: 1220
+//     },
+//     {
+//         name: "Rafaella Ballerini", 
+//         avatar: "https://avatars.githubusercontent.com/u/54322854?v=4", 
+//         whatsapp: "999999999", 
+//         bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+//         subject: "Química", 
+//         cost: "20", 
+//         weekday: [0], 
+//         time_from: 720, 
+//         time_to: 1220
+//     }
+// ];
+
+const { subjects, weekdays, getSubject, checkValues, convertHoursToMinutes } = require("./utils/format");
 
 server.get("/", function(req, res) {
     return res.render("index.html");
 })
-
-server.get("/study", function(req, res) {
+    
+server.get("/study", async function(req, res) {
     const filters = req.query;
-    return res.render("study.html", { proffys, filters, subjects, weekdays });
+    if (!filters.subject || !filters.weekday || !filters.time || filters.subject > subjects.length || filters.weekday > (weekdays.length - 1)) {
+        return res.render("study.html", { filters, subjects, weekdays });
+    }
+    const timeToMinutes = convertHoursToMinutes(filters.time);
+    const query = `
+        SELECT proffys.*, classes.* 
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id) 
+        WHERE EXISTS(
+            SELECT * FROM class_schedule WHERE class_id = classes.id AND weekday = ${filters.weekday} AND time_from <=  ${timeToMinutes} AND time_to > ${timeToMinutes}
+        )
+        AND classes.subject = ${filters.subject};
+    `
+    try {
+        const db = await Database;
+        const proffys = await db.all(query);
+        return res.render("study.html", { proffys, filters, subjects, weekdays });
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 server.get("/give-classes", function(req, res) {
@@ -66,10 +86,6 @@ server.get("/give-classes", function(req, res) {
     }
     return res.render("give-classes.html", { subjects, weekdays });
 })
-
-function getSubject(index) {
-    return subjects[+index - 1];
-}
 
 
 const PORT = process.env.PORT||"3000";
